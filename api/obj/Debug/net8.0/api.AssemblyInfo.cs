d4f671dd0e454a45b77d0fcd9dee7cfd2b643b4b
SayeedSanjana <sanjanasayeed68@gmil.com>
2024-11-08 01:54:21 -0500

    gitignore file added

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c28a287bdd8b63194894babca5d3283c499e2b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6f9d8dfac7910574476f7d06b021fb80595849")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
